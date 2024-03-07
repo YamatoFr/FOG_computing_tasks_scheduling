e32072b4db4930ca8d426a32db7e47da9d807e08
Théo FIGINI <60272310+YamatoFr@users.noreply.github.com>
2024-03-07 13:38:59 +0100

    deleted redundant files, slight script modification

--- scripts/launch.ps1
+++ scripts/launch.ps1
@@ -4,13 +4,16 @@ $reportFolder = "C:\Users\theof\OneDrive\Documents\Github\FOG_computing_tasks_sc
 $texFiles = Get-ChildItem -Path $reportFolder -Filter "*.tex" -File
 
 # Loop through each .tex file and launch citationcheck.py
-# with the first argument being the name of the .tex file
-# (except for rapport_para.tex, which is the main report file)
+# with the first argument being the name of the .tex file (except for rapport_para.tex, which is the main report file)
 # and the second argument being the mode of operation (1, 2 or 3. See citationcheck.py for details)
+
+# Input mode in console
+$mode = Read-Host "Enter the mode of operation (1, 2 or 3)"
+
 foreach ($texFile in $texFiles) {
 	if ($texFile.Name -ne "rapport_para.tex") {
 		$fileName = $texFile.Name.Replace(".tex", "")
-		python .\scripts\citationcheck.py $fileName 1
+		python .\scripts\citationcheck.py $fileName $mode
 		# print the name of the file to the console
 		Write-Host $fileName
 	}
